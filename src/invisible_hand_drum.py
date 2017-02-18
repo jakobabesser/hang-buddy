a0cc395fd7393ae537da1babff6e1c792e49a269
Jakob Abesser <nudelsalat@posteo.de>
2017-02-18 13:28:29 +0100

    Code cleanup iteration #1: new classes IHDPlayer, IHDPlayCommand, IHDGestureDetector

--- src/invisible_hand_drum.py
+++ src/invisible_hand_drum.py
@@ -88,27 +88,19 @@ class HandMemory:
         self.memory[_id]['hit_detected'] = False
 
 
-class VirtualHangDrum(Leap.Listener):
+class IHDController(Leap.Listener):
+    """ Main controller class """
 
     def __init__(self):
         Leap.Listener.__init__(self)
 
-        self.player = Player()
-
-        self.midi_out = rtmidi.MidiOut()
-        available_ports = self.midi_out.get_ports()
-
-        if available_ports:
-            self.midi_out.open_port(0)
-        else:
-            self.midi_out.open_virtual_port("hangbuddy")
+        self.player = IHDPlayer(self)
+        self.gesture_detector = IHDGestureDetector(self)
 
         self.silence_in_frames = 2
         self.silent_frames = 0
 
         self.start_time = time.time()
-        self.last_event_time = 0
-        self.reset_after_time = 2
 
         self.tempo_bpm = 110.
         self.numerator = 8
@@ -130,46 +122,17 @@ class VirtualHangDrum(Leap.Listener):
         self.click_pitch_beat_one = 48
         self.click_pitch_beat_other = 49
 
-        self.player = 'COMPUTER'
+        self.player.active_player = 'COMPUTER'
 
         # self.detection_method = 'key_tap'
         self.detection_method = 'manual'
-        self.hand_memory = HandMemory()
 
         self.bar_number = -1
 
         self.num_random_notes = 5
 
-        # heptagon layout for Hang drum
-        """
-                    P7
 
-              P5          P6
-                    P1
 
-              P3          P4
-
-                    P2
-
-        """
-
-        self.heptagon_positions_radius = 100
-        r1 = self.heptagon_positions_radius
-        r2 = np.sqrt(3)/2*r1
-        r1_2 = r1/2.
-
-        self.heptagon_positions_pitches = np.arange(36, 43)
-        self.heptagon_positions = np.array(((0, 0),
-                                            (0, r1),
-                                            (-r2, r1_2),
-                                            (r2, r1_2),
-                                            (-r2, -r1_2),
-                                            (r2, -r1_2),
-                                            (0, -r1)))
-
-        self.heptagon_positions = np.hstack((self.heptagon_positions, self.heptagon_positions_pitches[:, np.newaxis]))
-
-        self.frame_id = 0
 
     def on_init(self, controller):
         print "Initialized"
@@ -187,6 +150,7 @@ class VirtualHangDrum(Leap.Listener):
         # print('controller.config.set("Gesture.KeyTap.HistorySeconds"' + str(controller.config.get("Gesture.KeyTap.HistorySeconds")))
         # print('controller.config.set("Gesture.KeyTap.MinDistance"' + str(controller.config.get("Gesture.KeyTap.MinDistance")))
 
+        # todo place parameters to class arguments
         controller.config.set("Gesture.KeyTap.MinDownVelocity", 20)# 40.0)
         controller.config.set("Gesture.KeyTap.HistorySeconds", .1) #.2)
         controller.config.set("Gesture.KeyTap.MinDistance", 5)#1.0)
@@ -198,6 +162,44 @@ class VirtualHangDrum(Leap.Listener):
 
     def on_exit(self, controller):
         print "Exited"
+
+    def on_frame(self, controller):
+        """ Callback which is called every frame with leap motion controller data """
+
+        curr_time = time.time()
+
+        # get current data from motion sensor
+        frame = controller.frame()
+
+        # hand stroke detection
+        play_command = self.gesture_detector.analyze(frame, curr_time)
+
+        if play_command.pitch is not None:
+            self.player.play(play_command)
+
+
+        # start metronome after initial delay
+        if curr_time > self.start_time_first_beat and not self.metronome_started:
+            print('METRONOME STARTED')
+            self.metronome_started = True
+            self.metronome_start_time = curr_time
+
+        if self.metronome_started:
+            if self.last_bar_start_time is not None:
+                curr_time_in_beat = curr_time - self.last_bar_start_time
+                beats_passed = self.beat_times_in_bar < curr_time_in_beat
+                self.beats_passed[beats_passed] = True
+
+            # self.beat_times_in_bar = np.arange(self.numerator) * self.beat_duration / 2.
+            # self.beats_passed = np.zeros(self.numerator, dtype=bool)
+
+        if self.metronome_started:
+            self.check_for_beat()
+
+        self.player.update()
+
+
+
 
     def quantize_user_played_notes(self):
         self.quant_mat = np.zeros((7, self.numerator*2))
@@ -219,23 +221,28 @@ class VirtualHangDrum(Leap.Listener):
 
     def play_click(self):
         """ Play click sound depending on beat position """
+        play_command = IHDPlayCommand()
         if self.beat_idx == 0:
             self.bar_number += 1
             self.last_bar_start_time = time.time()
             self.beats_passed = np.zeros(self.numerator*2, dtype=bool)
             # switch between user and computer
             if self.bar_number % 2 == 1:
-                self.player = 'USER'
+                self.player.active_player = 'USER'
                 self.user_played_notes = []
             else:
-                self.player = 'COMPUTER'
+                self.player.active_player = 'COMPUTER'
                 self.quantize_user_played_notes()
-            print('%s PLAYS NOW!' % self.player)
+            print('%s PLAYS NOW!' % self.player.active_player)
 
-            self.play_note(self.click_pitch_beat_one, self.click_velocity)
-            # print('BAR NUMBER ' + str(self.bar_number))
+            play_command.pitch = self.click_pitch_beat_one
+            play_command.velocity = self.click_velocity
         else:
-            self.play_note(self.click_pitch_beat_other, self.click_velocity)
+            play_command.pitch = self.click_pitch_beat_other
+            play_command.velocity = self.click_velocity
+
+        self.player.play(play_command)
+
         self.beat_idx += 1
         self.beat_idx %= self.numerator
 
@@ -247,91 +254,68 @@ class VirtualHangDrum(Leap.Listener):
             self.play_click()
         self.prev_time_mod = curr_time_mod
 
-    def on_frame(self, controller):
-        """ Callback which is called every frame with controller data """
 
-        # reset hand memory after a while if nothing happens
-        curr_time = time.time()
-        if curr_time - self.last_event_time > self.reset_after_time:
-            self.hand_memory.reset_all()
+class IHDGestureDetector:
+    """ Main class to detect drumming gestures based on LeapMotion controller data """
+
+    def __init__(self, controller):
+        self.start_time = time.time()
+        self.last_event_time = 0
+        self.reset_after_time = 2
+        self.frame_id = 0
+        self.controller = controller
+
+        self.hand_memory = HandMemory()
+
+        # hexagon layout for Hang drum
+        """
+                    P7
+
+              P5          P6
+                    P1
+
+              P3          P4
+
+                    P2
+
+        """
+
+        self.hexagon_positions_radius = 100
+        r1 = self.hexagon_positions_radius
+        r2 = np.sqrt(3) / 2 * r1
+        r1_2 = r1 / 2.
+
+        self.hexagon_positions_pitches = np.arange(36, 43)
+        self.hexagon_positions = np.array(((0, 0),
+                                            (0, r1),
+                                            (-r2, r1_2),
+                                            (r2, r1_2),
+                                            (-r2, -r1_2),
+                                            (r2, -r1_2),
+                                            (0, -r1)))
+
+        self.hexagon_positions = np.hstack((self.hexagon_positions, self.hexagon_positions_pitches[:, np.newaxis]))
+
+    def analyze(self, frame, curr_time):
+        self.update_time(curr_time)
+        hand_stroke_position = self.detect_hand_stroke(frame)
+        play_command = IHDPlayCommand()
+
+        # if hand stroke was detected
+        if hand_stroke_position is not None:
             self.last_event_time = curr_time
-
-        # start metronome after initial delay
-        if curr_time > self.start_time_first_beat and not self.metronome_started:
-            print('METRONOME STARTED')
-            self.metronome_started = True
-            self.metronome_start_time = curr_time
-
-        if self.metronome_started:
-            if self.last_bar_start_time is not None:
-                curr_time_in_beat = curr_time - self.last_bar_start_time
-                beats_passed = self.beat_times_in_bar < curr_time_in_beat
-                self.beats_passed[beats_passed] = True
-
-            # self.beat_times_in_bar = np.arange(self.numerator) * self.beat_duration / 2.
-            # self.beats_passed = np.zeros(self.numerator, dtype=bool)
-
-        if self.metronome_started:
-            self.check_for_beat()
-
-        # Get the most recent frame and report some basic information
-        frame = controller.frame()
-
-        # (1) Manual hand stroke detection
-        if self.player == "COMPUTER":
-            beats_passed = np.where(self.beats_passed)[0]
-            if len(beats_passed) > 0:
-                last_beat_passed = beats_passed[-1]
-                active_pitches = np.where(self.quant_mat[:, last_beat_passed])[0]
-                if len(active_pitches) > 0:
-                    for pitch_idx in active_pitches:
-                        self.play_note(self.heptagon_positions_pitches[pitch_idx], 112)
-                        self.quant_mat[pitch_idx, last_beat_passed] = False
-                # print('active pitches ' + str(active_pitches))
-            # print(beats_passed)
-            # self.quant_mat
-
-
-            # print('qwdnoqwidhqowidh')
-            pass
-        else:
-            if self.detection_method == 'key_tap':
-                hand_stroke_detected = self.detect_hand_stroke_using_key_tap_gesture(frame)
-            elif self.detection_method == 'manual':
-                hand_stroke_detected = self.detect_hand_manual(frame)
-            else:
-                raise Exception('Non-valid detection method')
-
-            if hand_stroke_detected:
-                self.last_event_time = time.time()
-                self.reset_after_time
+            play_command.pitch = self.get_pitch_from_position(hand_stroke_position)
+            play_command.velocity = 1# todo replace by analyzing hand motion (velocity before stroke was detected)
 
         self.frame_id += 1
 
-    def detect_hand_stroke_using_key_tap_gesture(self, frame):
-        """ Method 1: Use leap motion key tap gesture as drum strokes
-            Performance: Misses some hits
-        """
-        # (2) Hand stroke detection using key tap gesture (performance: medium
+        return play_command
 
-        hand_stroke_detected = False
-
-        # Get gestures
-        for gesture in frame.gestures():
-
-            if gesture.type == Leap.Gesture.TYPE_KEY_TAP:
-
-                keytap = KeyTapGesture(gesture)
-                self.play_note_after_detected_hit(keytap.position)
-
-                hand_stroke_detected = True
-
-        return hand_stroke_detected
-
-    def detect_hand_manual(self, frame):
+    def detect_hand_stroke(self, frame):
         """ Manual detection of hand stroke motion of both hands
             Performance: much better :) """
-        hand_stroke_detected = False
+        hand_stroke_position = None
+
         # check that at least one hand is in the frame
         hands = frame.hands
         if len(hands) > 0:
@@ -340,88 +324,82 @@ class VirtualHangDrum(Leap.Listener):
                 position = hand.palm_position
 
                 if self.hand_memory.check_for_hand_stroke(_id, position, self.frame_id):
-                    self.play_note_after_detected_hit(position)
-                    hand_stroke_detected = True
-                # print "id %d, position: %s" % (hand.id, hand.palm_position)
-            # print('------')
+                    hand_stroke_position = position
 
-        return hand_stroke_detected
+        return hand_stroke_position
 
-    def get_pitch_from_position(self, position, heptagon_layout=True):
-        if heptagon_layout:
+    def update_time(self, curr_time):
+        # check for hand memory reset
+        if curr_time - self.last_event_time > self.reset_after_time:
+            self.hand_memory.reset_all()
+            self.last_event_time = curr_time
 
-            curr_pos = np.array((position[0], position[2]))
+    def get_pitch_from_position(self, position, hexagon_layout=True):
 
-            # nearest neighbor search
-            dist = np.sqrt(np.sum(np.square(self.heptagon_positions[:, :2] - curr_pos), axis=1))
+        curr_pos = np.array((position[0], position[2]))
 
-            pitch_id = np.argmin(dist)
-            pitch = self.heptagon_positions[pitch_id, 2]
+        # nearest neighbor search
+        dist = np.sqrt(np.sum(np.square(self.hexagon_positions[:, :2] - curr_pos), axis=1))
 
-            self.user_played_notes.append((time.time() - self.last_bar_start_time, pitch_id))
-        else:
+        pitch_id = np.argmin(dist)
+        pitch = self.hexagon_positions[pitch_id, 2]
 
-            print position
-            left, right, front, back = False, False, False, False
-            if position[0] < -10:
-                left = True
-            elif position[0] > -10:
-                right = True
-            if position[2] < 50: #5:
-                back = True
-            elif position[2] > 50:#10:
-                front = True
-            is_low = True
-            if position[1] > 150:
-                is_low = False
+        self.controller.user_played_notes.append((time.time() - self.controller.last_bar_start_time, pitch_id))
 
-            pitch = None
-            if left and front:
-                pitch = 36
-            elif left and back:
-                pitch = 37
-            elif right and back:
-                pitch = 38
-            elif right and front:
-                pitch = 39
-            if pitch is not None and not is_low:
-                pitch += 4
-
-            print "left %d right %d front %d back %d - low %d" % (left, right, front, back, is_low)
         return pitch
 
-    def play_note_after_detected_hit(self, position):
-        pitch = self.get_pitch_from_position(position)
 
-        if pitch is not None:
-            self.play_note(pitch)
+class IHDPlayCommand:
+
+    def __init__(self, pitch=None, velocity=None):
+        self.pitch = pitch
+        self.velocity = velocity
+
+
+class IHDPlayer:
+
+    def __init__(self, controller):
+        self.controller = controller
+
+        self.midi_out = rtmidi.MidiOut()
+        available_ports = self.midi_out.get_ports()
+
+        if available_ports:
+            self.midi_out.open_port(0)
         else:
-            print 'Gesture not valid'
+            self.midi_out.open_virtual_port("virtual_hand_drum")
 
-
-    def play_note(self, pitch=None, velocity=112):
-        note_on = [0x90, pitch, velocity]  # channel 1, middle C, velocity 112
-        self.midi_out.send_message(note_on)
-
-
-class Player:
-
-    def __init__(self):
         self.scale = ''
+        self.active_player = None
         pass
 
+    def update(self):
+
+        if self.active_player == "COMPUTER":
+            beats_passed = np.where(self.controller.beats_passed)[0]
+            if len(beats_passed) > 0:
+                last_beat_passed = beats_passed[-1]
+                active_pitches = np.where(self.controller.quant_mat[:, last_beat_passed])[0]
+                if len(active_pitches) > 0:
+                    for pitch_idx in active_pitches:
+                        play_command = IHDPlayCommand(self.controller.gesture_detector.hexagon_positions_pitches[pitch_idx], 1)
+                        self.controller.player.play(play_command)
+                        self.controller.quant_mat[pitch_idx, last_beat_passed] = False
+
     def change_scale(self, scale):
+        # TODO implement me
         self.scale = scale
 
-
-
-
+    def play(self, play_command):
+        velocity = int(122.*play_command.velocity)
+        note_on = [0x90, play_command.pitch, play_command.velocity]  # channel 1, middle C, velocity 112
+        self.midi_out.send_message(note_on)
 
 
 def main():
 
     # Create a sample listener and controller
-    listener = VirtualHangDrum()
+    listener = IHDController()
     controller = Leap.Controller()
 
     # Have the sample listener receive events from the controller
